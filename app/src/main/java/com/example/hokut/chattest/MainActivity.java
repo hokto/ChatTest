ee51dc408a25178d3b8d8a57379f21ec2e544cbd
hokut <hokuto.k1441@gmail.com>
2019-03-10 20:31:14 +0900

    first Commit

--- app/src/main/java/com/example/hokut/chattest/MainActivity.java
+++ app/src/main/java/com/example/hokut/chattest/MainActivity.java
@@ -28,28 +28,28 @@ public class MainActivity extends AppCompatActivity {
 
     private WebSocketClient mClient;
     public String id;
-    public final void MakeMessage(final String message_log)
+    public final void MakeMessage(final String message_log)//TextViewを動的に生成する関数
     {
         LinearLayout linearLayout=findViewById(R.id.layout);
         TextView textView=new TextView(this);
-        String name=message_log.substring(0,message_log.indexOf(":"));
-        String message=message_log.substring(message_log.indexOf(":")+1,message_log.length());
+        String name=message_log.substring(0,message_log.indexOf(":"));//サーバから受信したIDのみ取得
+        String message=message_log.substring(message_log.indexOf(":")+1,message_log.length());//入力された文字列のみ取得
         textView.setText(message);
         textView.setWidth(100);
         textView.setHeight(50*((message.length()/16)+1));
         RelativeLayout.LayoutParams param = new RelativeLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         if(name.equals(id))
         {
-            param.setMargins(600,0,0,0);
+            param.setMargins(600,0,0,0);//IDが自分のものであれば位置を調整
         }
-        linearLayout.addView(textView,param);
+        linearLayout.addView(textView,param);//テキスト追加
     }
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setIDconfig();
     }
-    public void setIDconfig()
+    public void setIDconfig()//IDを入力させる画面の表示
     {
         setContentView(R.layout.activity_idconfig);
         Button id_btn=findViewById(R.id.ID_btn);
@@ -59,22 +59,21 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v)
             {
-                id=id_edit.getText().toString();
-                setMainWindow();
+                id=id_edit.getText().toString();//入力したIDを文字列として取得
+                setMainWindow();//画面遷移
             }
         });
     }
-    public void setMainWindow()
+    public void setMainWindow()//チャットさせる画面
     {
         setContentView(R.layout.activity_main);
         mHandler = new Handler();
         if ("sdk".equals(Build.PRODUCT)) {
-            // エミュレータの場合はIPv6を無効    ----1
             java.lang.System.setProperty("java.net.preferIPv6Addresses", "false");
             java.lang.System.setProperty("java.net.preferIPv4Stack", "true");
         }
         try {
-            URI uri = new URI("ws://192.168.0.23:8888");
+            URI uri = new URI("ws://192.168.0.23:8888");//サーバーのIPアドレス、ポート番号
             mClient = new WebSocketClient(uri) {
                 @Override
                 public void onOpen(ServerHandshake handshake) {
@@ -82,14 +81,13 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 @Override
-                public void onMessage(final String message) {
+                public void onMessage(final String message) {//サーバからのデータ受信
                     Log.d(TAG, "onMessage");
                     Log.d(TAG, "Message:" + message);
-                    mHandler.post(new Runnable() {    // ----2
+                    mHandler.post(new Runnable() {
                         @Override
                         public void run() {
                             MakeMessage(message);
-                            //Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
                         }
                     });
                 }
@@ -105,20 +103,18 @@ public class MainActivity extends AppCompatActivity {
                     Log.d(TAG, "onClose");
                 }
             };
-            mClient.connect();
+            mClient.connect();//サーバーに接続
 
         } catch (URISyntaxException e) {
             e.printStackTrace();
         }
-        // 送信ボタン
-        Button button =  findViewById(R.id.ID_btn);
+        Button button =  findViewById(R.id.ID_btn);//送信用のボタン
         button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                EditText edit =  findViewById(R.id.text);
+            public void onClick(View v) {//クリックされたらIDと入力された文字列を送信
+                EditText edit =  findViewById(R.id.text);//文字入力するところ
                 try {
-                    // 送信
-                    mClient.send(id+":"+edit.getText().toString());
+                    mClient.send(id+":"+edit.getText().toString());//データ送信
                 } catch (NotYetConnectedException e) {
                     e.printStackTrace();
                 }/* catch (InterruptedException e) {
